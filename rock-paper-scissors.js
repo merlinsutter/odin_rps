20e23c5efeb97b37f13f0a32b1d6601d9c34806c
Merlin Sutter <merlin_sutter@icloud.com>
2018-03-08 23:51:37 +0000

    Initial commit

---
+++ rock-paper-scissors.js
@@ -0,0 +1,12 @@
+// Randomly return either 'Rock', 'Paper', or 'Scissors'
+function computerPlay() {
+  var options = ['Rock', 'Paper', 'Scissors']
+  let choice = options[Math.floor(Math.random()*options.length)];
+  console.log("Computer selects: " + choice);
+  return choice;
+}
+
+// Play a round of Rock, Paper, scissors
+function playRound(playerSelection, computerSelection {
+
+}
