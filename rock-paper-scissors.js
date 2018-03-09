de884c3e12193d65c277eeabb30742a1b38014c6
Merlin Sutter <merlin_sutter@icloud.com>
2018-03-09 20:41:05 +0000

    Move options array out of computerPlay function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,7 +1,29 @@
+const options = ['Rock', 'Paper', 'Scissors']
+
 // Randomly return either 'Rock', 'Paper', or 'Scissors'
 function computerPlay() {
-  let options = ['Rock', 'Paper', 'Scissors']
   let choice = options[Math.floor(Math.random()*options.length)];
   console.log("Computer selects: " + choice);
   return choice;
+}
+
+// Play one round of Rock, Paper, scissors
+function playRound(playerSelection, computerSelection)
+{
+  playerSelection = prompt('What do you choose?');
+  computerSelection = computerPlay();
+
+  let win;
+
+  if (playerSelection === computerSelection)
+  {
+
+  }
+
+  if (win)
+  {
+    alert(`You win! ${playerSelection} beats ${computerSelection}`);
+  } else {
+    alert(`You lose! ${computerSelection} beats ${playerSelection}`);
+  }
 }
