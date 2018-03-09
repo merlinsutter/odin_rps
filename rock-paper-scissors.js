f7b18f63ec6722db5899d9e5179b93470e8b1f30
Merlin Sutter <merlin_sutter@icloud.com>
2018-03-09 00:15:36 +0000

    computerPlay function works

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,12 +1,7 @@
 // Randomly return either 'Rock', 'Paper', or 'Scissors'
 function computerPlay() {
-  var options = ['Rock', 'Paper', 'Scissors']
+  let options = ['Rock', 'Paper', 'Scissors']
   let choice = options[Math.floor(Math.random()*options.length)];
   console.log("Computer selects: " + choice);
   return choice;
-}
-
-// Play a round of Rock, Paper, scissors
-function playRound(playerSelection, computerSelection {
-
 }
